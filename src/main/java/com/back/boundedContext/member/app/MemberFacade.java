f709fe1b89fa015714b95b7221b7e0f7ec58dbbe
kimsohee <thgml0716030@naver.com>
2026-09-17 17:39:24 +0900

    Member RsData 도입 작성 후 결과코드와 메세지도 같이 반환

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -4,6 +4,7 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.exception.DomainException;
 
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,7 +23,7 @@ public class MemberFacade {
     }
 
     @Transactional(readOnly = true)
-    public Member join(String username, String password, String nickname) {
+    public RsData<Member> join(String username, String password, String nickname) {
 
 
         return memberJoinUseCase.join(username, password, nickname);
