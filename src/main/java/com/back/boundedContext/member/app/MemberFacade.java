0cf9bf204429d2427b84ec565bddb0b77dfad535
kimsohee <thgml0716030@naver.com>
2026-09-21 11:20:14 +0900

    0029

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -3,7 +3,6 @@ package com.back.boundedContext.member.app;
 import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.member.domain.MemberPolicy;
 import com.back.boundedContext.member.out.MemberRepository;
-import com.back.global.exception.DomainException;
 
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
@@ -15,33 +14,33 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class MemberFacade {
-    private final MemberRepository memberRepository;
+    private final MemberSupport memberSupport;
     private final MemberJoinUseCase memberJoinUseCase;
     private final MemberPolicy memberPolicy;
+    private final MemberGetRandomSecureTipUseCase memberGetRandomSecureTipUseCase;
 
-    @Transactional(readOnly = true)
-    public long count() {
-        return memberRepository.count();
-    }
 
     @Transactional(readOnly = true)
     public RsData<Member> join(String username, String password, String nickname) {
-
-
         return memberJoinUseCase.join(username, password, nickname);
+    }
+    public String getRandomSecureTip() {
+        return memberGetRandomSecureTipUseCase.getRandomSecureTip();
+    }
+    @Transactional(readOnly = true)
+    public long count() {
+        return memberSupport.count();
     }
 
     @Transactional(readOnly = true)
     public Optional<Member> findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+        return memberSupport.findByUsername(username);
     }
+
 
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id){
-        return memberRepository.findById(id);
+        return memberSupport.findById(id);
     }
 
-    public String getRandomSecureTip() {
-        return "비밀번호와 유효기간은 %d입니다.".formatted(memberPolicy.getNeedToChangePasswordDays());
-    }
 }
